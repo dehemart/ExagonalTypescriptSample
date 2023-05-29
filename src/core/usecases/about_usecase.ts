3d7803982fb826af3d80f63c50a0495fe46e1493
Denis Martins <martins.denis@gmail.com>
2023-05-29 17:22:00 -0300

    add initial code

--- src/core/usecases/about_usecase.ts
+++ src/core/usecases/about_usecase.ts
@@ -2,7 +2,7 @@ import { AboutInterface } from '../ports/inbound/about_interface';
 
 export class AboutUsecase implements AboutInterface {
   getTextAbout(): string {
-    return 'This is about text to application!';
+    return 'This is an about text to application!';
   }
 
 }
