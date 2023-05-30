25cc32f879af3ce570dd4ab473eded26ad0c48c2
Denis Martins <martins.denis@gmail.com>
2023-05-30 16:15:15 -0300

    adding prisma

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import 'dotenv/config';
+import * as dotenv from 'dotenv';
 import express from 'express';
 import { App } from './app';
 
@@ -7,10 +7,13 @@ export class Server {
   private server_port = Number( process.env.SERVER_PORT || '8880' );
 
   constructor() {
+    dotenv.config();
     this.server = new App().execute();
   }
 
   execute() {
+    console.log( process.env.DATABASE_URL );
+
     this.server.listen( this.server_port, () => {
       console.log( `Server running at ${this.server_port} port ......` );
     } );
